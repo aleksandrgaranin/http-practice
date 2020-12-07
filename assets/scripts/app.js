ad972d92cf44e016ed51f9d82426c7f93f28431c
ALEKS G <aleksandrgaranin89@gmail.com>
2020-12-07 17:40:43 -0600

    logic connected to UI

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,8 @@
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
+const form = document.querySelector('#new-post form');
+const fetchButton = document.querySelector('#available-posts button');
+
 
 function sendHttpRequest(method, url, data) {
     const promise = new Promise((resolve, reject) => {
@@ -53,5 +56,14 @@ async function cretePost(title, content) {
 }
 
 
-fetchPosts();
-cretePost('Dummy', 'Dummy Data');
+
+// cretePost('Dummy', 'Dummy Data');
+
+fetchButton.addEventListener('click', fetchPosts)
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    const enteredTitle = event.currentTarget.querySelector('#title').value
+    const enteredContent = event.currentTarget.querySelector('#content').value
+
+    cretePost(enteredTitle, enteredContent)
+})
