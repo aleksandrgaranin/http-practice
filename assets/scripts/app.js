85b8afac97a8d21edb54f687ea03866b3f1e4285
ALEKS G <aleksandrgaranin89@gmail.com>
2020-12-07 17:22:24 -0600

    Promisifying Http Requests

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,28 +1,44 @@
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
 
-const xhr = new XMLHttpRequest();
+function sendHttpRequest(method, url) {
+    const promise = new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url); // 'https://jsonplaceholder.typicode.com/posts'
+        xhr.responseType = 'json' // automaticly parsing JSON in to JavaScript Object
+        xhr.onload = function () {
+            resolve(xhr.response)
+        };
+        xhr.send();
+    });
+    return promise;    
+}
 
+// function fetchPosts() {
+//     sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts')
+//         .then(responseData => {
+//             console.log(responseData)
+//             const listOfPosts = responseData
+//             for (const post of listOfPosts){
+//                 const postEl = document.importNode(postTemplate.content, true);
+//                 postEl.querySelector('h2').textContent = post.title.toUpperCase();
+//                 postEl.querySelector('p').textContent = post.body;
+//                 listElement.append(postEl)
+//             }
+//         }
+//     )
+// }
 
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+// OR
 
-xhr.responseType = 'json' // automaticly parsing JSON in to JavaScript Object
-
-xhr.onload = function () {
-    // console.log(xhr.response)
-    // const listOfPosts = JSON.parse(xhr.response) // transform JSON in to JavaScript Object
-    const listOfPosts = xhr.response
-    console.log(listOfPosts)
-
-    for (const post of listOfPosts){
+async function fetchPosts() {
+    const responseData = await sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts');    
+    for (const post of responseData){
         const postEl = document.importNode(postTemplate.content, true);
         postEl.querySelector('h2').textContent = post.title.toUpperCase();
         postEl.querySelector('p').textContent = post.body;
         listElement.append(postEl)
-    }
-};
+    }           
+}
 
-xhr.send()
-
-
-
+fetchPosts()
